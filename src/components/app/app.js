e1298a5c979e17009dc4efd92b1143f96d672cf2
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-03-30 10:20:30 +0300

    handleMenuClick

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,6 +14,7 @@ export class AppController {
   handleTaskAdd(task) {
     this.tasks.push(task);
   }
+  openDrawer() {}
 }
 
 export const appModule = angular
--- src/components/toolbar/toolbar.js
+++ src/components/toolbar/toolbar.js
@@ -1,10 +1,15 @@
 import angular from "angular";
 import template from "./toolbar.html";
-
+class ToolbarController {
+  handleMenuClick() {
+    this.onMenuClick();
+  }
+}
 export const toolbarModule = angular
   .module("toolbar", ["ngMaterial"])
   .component("toolbar", {
     template,
+    controller: ToolbarController,
     bindings: {
       onMenuClick: "&"
     }
