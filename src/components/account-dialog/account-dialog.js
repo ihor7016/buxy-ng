d0915d30609d7fd6479be0438f11d344f8872caf
gift-a <ksenia.gift@gmail.com>
2018-03-30 22:13:12 +0300

    account dialog

---
+++ src/components/account-dialog/account-dialog.js
@@ -0,0 +1,75 @@
+import angular from "angular";
+import template from "./account-dialog.html";
+
+import ngAnimate from "angular-animate";
+import ngMaterial from "angular-material";
+
+export class AccountDialogController {
+  constructor($mdDialog) {
+    this.$mdDialog = $mdDialog;
+    this.types = [
+      "checking",
+      "savings",
+      "credit card",
+      "cash",
+      "investiment",
+      "loan",
+      "cd",
+      "real estate",
+      "vehicle",
+      "insurance",
+      "other"
+    ];
+    this.currencies = ["UAH", "USD", "EUR"];
+  }
+
+  $onChanges(changes) {
+    if (changes.show.currentValue) {
+      this.showDialog(changes.show.currentValue.event);
+    }
+  }
+
+  showDialog(ev) {
+    this.$mdDialog.show({
+      template,
+      controllerAs: "$ctrl",
+      controller: this.dialogController(),
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true
+    });
+  }
+  dialogController() {
+    const self = this;
+    return class {
+      constructor($mdDialog) {
+        this.$mdDialog = $mdDialog;
+        this.types = self.types;
+        this.currencies = self.currencies;
+      }
+
+      submit() {
+        console.log({
+          name: this.account,
+          balance: this.balance,
+          type: this.type,
+          currency: this.currency
+        });
+        this.$mdDialog.hide();
+      }
+
+      abort() {
+        this.$mdDialog.cancel();
+      }
+    };
+  }
+}
+
+export const accountDialogModule = angular
+  .module("accountDialogModule", [ngMaterial, ngAnimate])
+  .component("accountDialog", {
+    controller: AccountDialogController,
+    bindings: {
+      show: "<"
+    }
+  }).name;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,23 @@
 import angular from "angular";
 import template from "./app.html";
 
-export const appModule = angular.module("appModule", []).component("app", {
-  template
-}).name;
+import ngMaterial from "angular-material";
+
+import { accountDialogModule } from "../account-dialog/account-dialog";
+
+export class AppController {
+  constructor() {
+    this.accountDialogShow;
+  }
+
+  onAddAccountClick(event) {
+    this.accountDialogShow = { event: event };
+  }
+}
+
+export const appModule = angular
+  .module("appModule", [ngMaterial, accountDialogModule])
+  .component("app", {
+    template,
+    controller: AppController
+  }).name;
