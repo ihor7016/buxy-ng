97bdf1da0a6dba598776fbf08952f2459d9c408d
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-01 13:12:58 +0300

    dinamic accounts list

--- src/components/accounts/accounts.js
+++ src/components/accounts/accounts.js
@@ -1,6 +1,32 @@
 import angular from "angular";
 import template from "./accounts.html";
 class AccountsController {
+  constructor() {
+    this.accounts = [
+      {
+        name: "ssss",
+        balance: 2222,
+        type: "checking",
+        currency: "UAH",
+        id: "zbv873vg6cm"
+      },
+      {
+        name: "ddd",
+        balance: 111,
+        type: "checking",
+        currency: "UAH",
+        id: "4rrhekqnwev"
+      },
+      {
+        name: "reretr",
+        balance: 2222,
+        type: "checking",
+        currency: "UAH",
+        id: "i78h0bobom"
+      }
+    ];
+  }
+
   addAccountItemClick() {}
 }
 export const accountsModule = angular
