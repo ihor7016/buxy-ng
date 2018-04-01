44abe4e60dc0c532dddb166525ae4f5d197aaee5
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-01 12:44:07 +0300

    add list button

--- src/components/drawer/drawer.js
+++ src/components/drawer/drawer.js
@@ -4,14 +4,16 @@ import template from "./drawer.html";
 export class DrawerController {
   constructor($mdSidenav) {
     this.$mdSidenav = $mdSidenav;
+    this.isOpened = false;
   }
+  addAccountClick(event) {}
 
   $onChanges(changes) {
     this.toggleLeft();
   }
 
   toggleLeft() {
-    this.$mdSidenav("left").toggle();
+    this.isOpened = !this.isOpened;
   }
 }
 export const drawerModule = angular
