22b92b7be193db352cb5234bff4b02ede066fb34
gift-a <ksenia.gift@gmail.com>
2018-04-03 17:09:13 +0300

    tag dialog impl

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,29 @@
 import angular from "angular";
 import template from "./app.html";
 
-export const appModule = angular.module("appModule", []).component("app", {
-  template
-}).name;
+import ngMaterial from "angular-material";
+
+import { tagDialogModule } from "../tag-dialog/tag-dialog";
+
+export class AppController {
+  handleAddTagClick(event) {
+    this.isTagDialogOpen = true;
+    this.tagDialogOpenEvent = event;
+  }
+
+  handleTagDialogSubmit(data) {
+    console.log(data);
+    this.isTagDialogOpen = false;
+  }
+
+  handleTagDialogClose() {
+    this.isTagDialogOpen = false;
+  }
+}
+
+export const appModule = angular
+  .module("appModule", [ngMaterial, tagDialogModule])
+  .component("app", {
+    template,
+    controller: AppController
+  }).name;
