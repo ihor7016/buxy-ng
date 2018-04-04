31c5a5d86928ca743d3d8d9938afe7bdf5aba2df
gift-a <ksenia.gift@gmail.com>
2018-04-04 16:57:54 +0300

    separate file firebase init

---
+++ firebase.init.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyCGpm5s4Fvwr40RLpEpjI_Wa1xksPSJb9s",
+  authDomain: "buxy-ng.firebaseapp.com",
+  databaseURL: "https://buxy-ng.firebaseio.com",
+  projectId: "buxy-ng",
+  storageBucket: "",
+  messagingSenderId: "395078643655"
+};
+firebase.initializeApp(config);
