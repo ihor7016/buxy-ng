33f292061056f9b790864d059a9b930a4ed8b814
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-05 00:38:40 +0300

    removed controller and returned div

--- src/components/button-more/button-more.js
+++ src/components/button-more/button-more.js
@@ -6,15 +6,10 @@ import template from "./button-more.html";
 
 import { popupModule } from "./popup/popup";
 
-export class ButtonMoreController {
-  constructor() {}
-}
-
 export const buttonMoreModule = angular
   .module("buttonMore", ["ngMaterial", popupModule])
   .component("buttonMore", {
     template,
-    controller: ButtonMoreController,
     bindings: {
       position: "@"
     }
