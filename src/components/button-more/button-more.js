2b713e41b2035e03535644791f570d079a2a9329
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-04-05 10:49:23 +0300

    popup component

--- src/components/button-more/button-more.js
+++ src/components/button-more/button-more.js
@@ -3,7 +3,7 @@ import ngAria from "angular-aria";
 import ngMaterial from "angular-material";
 import template from "./button-more.html";
 
-import { popupModule } from "./popup/popup";
+import { popupModule } from "../popup/popup";
 
 export const buttonMoreModule = angular
   .module("buttonMore", [ngMaterial, ngAria, popupModule])
